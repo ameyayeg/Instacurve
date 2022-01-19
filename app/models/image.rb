22562f1c3060c95318794c71071622b6f56274c7
YOUR NAME <ameya.charnalia@gmail.com>
2022-01-19 10:41:57 +0000

    Added image validation

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,13 @@ class Image < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+  validate :correct_image_type
 
+  def correct_image_type
+    if image.attached? && !image.content_type.in?(%w(image/jpeg image/png))
+      errors.add(:image, 'Must be a JPEG or PNG')
+    elsif image.attached? == false
+      errors.add(:image, 'Image required')
+    end
+  end
 end

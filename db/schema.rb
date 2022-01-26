ee03d69a4aa1045324fa1a4c121d043de2752ba2
YOUR NAME <ameya.charnalia@gmail.com>
2022-01-26 18:14:22 +0000

    Update schema and add procfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_13_115736) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 2022_01_13_115736) do
     t.string "content_type"
     t.text "metadata"
     t.string "service_name", null: false
-    t.integer "byte_size", null: false
+    t.bigint "byte_size", null: false
     t.string "checksum"
     t.datetime "created_at", precision: 6, null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
@@ -41,7 +44,7 @@ ActiveRecord::Schema.define(version: 2022_01_13_115736) do
   end
 
   create_table "images", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
